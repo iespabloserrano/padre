f53742828aa66d6c0770ae8c66874f6e8da94bb0
JoTerrance <gsanz@iespabloserrano.com>
2026-01-15 13:40:07 +0100

    added herbivore and carnivore classes to explain delegatyion inheritance

--- models.py
+++ models.py
@@ -7,3 +7,24 @@ class Person(models.Model):
 
     name = fields.Char(string='Name', required=True)
     age = fields.Integer(string='Age')
+
+
+class Herbivore(models.Model):
+    _name = 'padre.herbivore'
+    _description = 'Herbivore'
+
+
+    favorite_plant = fields.Char(string='Favorite Plant')
+
+    def get_favorite_plant(self):
+        return self.favorite_plant
+    
+class Carnivore(models.Model):
+    _name = 'padre.carnivore'
+    _description = 'Carnivore'
+
+
+    favorite_meat = fields.Char(string='Favorite Meat')
+
+    def get_favorite_meat(self):
+        return self.favorite_meat
